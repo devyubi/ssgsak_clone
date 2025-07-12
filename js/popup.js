2be5f34b1fab96984fe84c6aa6d59609a1e24d2a
dev.munyubi <dev.munyubi@gmail.com>
2025-07-12 12:23:18 +0900

    [docs] popup 완료

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
   const closeBt = document.querySelector(".popup_close");
   const popup = document.querySelector(".popup");
+  const closeX = document.querySelector(".popup_x");
 
+  closeX.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
   closeBt.addEventListener("click", () => {
     popup.style.display = "none";
   });
